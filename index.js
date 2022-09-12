5aaad1249bee43147716823a248a29cca9b12ed6
Ameenaminah <aminatameen111@gmail.com>
2022-09-12 22:46:28 +0100

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let num1 = 15;
+let num2 = 5
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+let sumEl = document.getElementById("sum-el");
+
+function add() {
+    let total = num1 + num2;
+    sumEl.textContent = "Sum: " + total
+}
+function subtract() {
+    let total = num1 - num2;
+    sumEl.textContent = "Sum: " + total
+}
+function divide() {
+    let total = num1 / num2;
+    sumEl.textContent = "Sum: " + total
+}
+function multiply() {
+    let total = num1 * num2;
+    sumEl.textContent = "Sum: " + total
+}
